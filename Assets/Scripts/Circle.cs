33164268784f9eb760d1cfb28c73632de1bf6611
ChangeCaps <hjalte.nannestad@gmail.com>
2019-05-09 16:37:26 +0200

    things

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Circle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckPosition(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckPosition(int x, int y)
+    {
+        if (x == 0 && y == 0)
+        {
+
+        }
+
+        x += 60;
+        y += 60;
+
+        foreach (Image g in GetComponentsInChildren<Image>())
+        {
+            if (g.sprite.texture.GetPixel(x + g.sprite.texture.height * g.GetComponent<CirclePart>().num, y).a != 0)
+            {
+                g.gameObject.SetActive(false);
+            }
+        }
+    }
+}
